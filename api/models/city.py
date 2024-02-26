b1aa42bd4e619e4169f2ff1dc4d92a8b51428172
Phiwo Njemla <phiwo041590@gmail.com>
2024-02-26 14:48:12 -0800

    Airbnb_clon3_v3

---
+++ api/models/city.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Holds class City"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
+
+
+class City(BaseModel, Base):
+    """Representation of the city """
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'cities'
+        name = Column(String(128),
+                      nullable=False)
+        state_id = Column(String(60),
+                          ForeignKey('states.id'),
+                          nullable=False)
+        places = relationship("Place",
+                              backref="cities",
+                              cascade="all, delete-orphan")
+    else:
+        name = ""
+        state_id = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initializes city"""
+        super().__init__(*args, **kwargs)
---
+++ api/v1/views/index.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""index.py connect to API"""
+from api.v1.views import app_views
+from flask import Flask, Blueprint, jsonify
+from models import storage
+
+
+hbnbText = {
+    "amenities": "Amenity",
+    "cities": "City",
+    "places": "Place",
+    "reviews": "Review",
+    "states": "State",
+    "users": "User"
+}
